aaf476a02e4af0b41afe734e2ed3805dbb122a15
Jonah Gile <jonah.gile@edu.sait.ca>
2025-09-23 12:58:43 -0600

    linkstyles
    created linkStyles for assignment links

--- app/page.js
+++ app/page.js
@@ -1,22 +1,24 @@
 import Link from "next/link";
 
 export default function Home() {
+  let linkStyles = "text-blue-600 underline";
+
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - assignments</h1>
       <ul>
         <li>
-          <Link className="text-blue-600 underline" href="./week-2">
+          <Link className={linkStyles} href="./week-2">
             Week 2 Assignment
           </Link>
         </li>
         <li>
-          <Link className="text-blue-600 underline" href="./week-3">
+          <Link className={linkStyles} href="./week-3">
             Week 3 Assignment
           </Link>
         </li>
         <li>
-          <Link className="text-blue-600 underline" href="./week-4">
+          <Link className={linkStyles} href="./week-4">
             Week 4 Assignment
           </Link>
         </li>
